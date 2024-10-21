9cbd284aac324bd3d7dd5acf960c13f39e278ead
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-10-21 15:11:09 +0200

    My first commit for the amenity.py with the getter/setter method

---
+++ part2/hbnb/app/models/amenity.py
@@ -0,0 +1,22 @@
+from base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel."""
+    def __init__(self, name):
+        super().__init__()
+        self._name = name
+        self.validate()
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 50:
+            raise ValueError("Amenity name exceeds maximum length")
+        self._name = value
+
+    def validate(self):
+        self.name = self._name
