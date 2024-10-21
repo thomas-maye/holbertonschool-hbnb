f6584e838cb07faecf0536eb5e7dbad753385030
Mezanic <ezanic.mael@gmail.com>
2024-10-21 10:22:20 +0200

    First implmentation of Users class

---
+++ part2/hbnb/app/models/user.py
@@ -0,0 +1,24 @@
+from base_model import BaseModel
+"""Module Define Users class """
+
+
+class Users(BaseModel):
+    """Users class based on BaseModel class"""
+
+    def __init__(self, first_name, last_name, email, is_admin):
+        """Constructor for Users class"""
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.admin = is_admin
+        self.places = []  # List to store related Places
+        self.reviews = []  # List to store related reviews
+
+    def add_review(self, review):  # Need to know how to link this with place before (WIP)
+        """Add a review from a places to the places owner."""
+        self.reviews.append(review)
+
+    def add_place(self, place):
+        """Add a places to the places."""
+        self.places.append(place)
