cea624f97c8cd4da8e1098e26ed1badfcb3276d3
Mezanic <ezanic.mael@gmail.com>
2024-11-14 12:27:00 +0100

    Delete pycache, updaye gitignore  and first update for task2

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+jwt = JWTManager()
+bcrypt = Bcrypt()
+
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
-from flask_bcrypt import Bcrypt
+from app.api.v1.auth import api as auth_ns
 
-bcrypt = Bcrypt()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -19,6 +24,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     bcrypt.init_app(app)
+    jwt.init_app(app)
     return app
