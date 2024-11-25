3cc5bce792a582da6bb0ea8b1e561af5deb23f36
Mezanic <ezanic.mael@gmail.com>
2024-11-25 12:01:34 +0100

    Create db when we run the server

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -43,4 +43,6 @@ def create_app(config_class=config.DevelopmentConfig):
     bcrypt.init_app(app)
     jwt.init_app(app)
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
     return app
